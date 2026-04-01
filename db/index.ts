305f5eba1558ec66962d1c65c6e6f17a8af9a8d4
shun2218-dev <kohaku1015game@gmail.com>
2026-04-01 11:12:39 +0900

    chore: add Drizzle query performance logger (#146)
    - Add db/logger.ts with PerformanceLogger class
    - Development: logs all queries with truncated SQL
    - Production: warns on slow queries (> 500ms threshold)
    - Enable logger in db/index.ts drizzle() config

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,9 @@
 import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
+import { PerformanceLogger } from "./logger";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, {
+  schema,
+  logger: new PerformanceLogger(),
+});
